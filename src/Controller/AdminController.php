9bad2649e795e1c65eafef0a724581fbd9ec3bff
John Doe <didimilca@hotmail.fr>
2019-07-02 09:14:01 +0200

    mardi matin

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+
+        /*
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+
+
+        */
+        return $this->render('admin/index.html.twig');
+    }
+
+
+
+    public function adminDashboard()
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // or add an optional message - seen by developers
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'User tried to access a page without having ROLE_ADMIN');
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -4,7 +4,64 @@
 namespace App\Controller;
 
 
-class TagController
+use App\Entity\Tag;
+use App\Form\TagType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TagController extends AbstractController
 {
+
+
+
+
+
+    /**
+     * Affiche et traite le formulaire d'ajout d'un Tag
+     * @Route("/tag/creation", methods={"GET", "POST"})
+     * @param Request $requestHTTP
+     * @return Response
+     */
+    public function create(Request $requestHTTP):Response
+    {
+
+        // Recuperation du formulaire
+        $tag = new Tag();
+        $formTag = $this->createForm(TagType::class, $tag);
+
+
+        // On envoie les données postées au formulaire
+        $formTag->handleRequest($requestHTTP);
+
+
+        // On vérifie que le formulaire est soumis et valide
+        if ($formTag->isSubmitted() && $formTag->isValid()) {
+            // On sauvegarde le produit en BDD grâce au manager
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($tag);
+            $manager->flush();
+
+            // Ajout d'un message flash
+            $this->addFlash('success', 'Le Tag a bien été ajouté');
+
+            // Redirection
+            //return $this->redirectToRoute('app_product_index');
+        }
+
+        return $this->render('tag/create.html.twig', [
+            'formTag' => $formTag->createView()
+        ]);
+
+
+
+
+
+    }
+
+
+
+
 
 }
